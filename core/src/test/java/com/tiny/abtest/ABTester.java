875bb7824dfccdf20115eb5ab49525808629f911
tiny <tiny_wcl@163.com>
2016-11-14 10:36:28 +0800

    commit test

--- core/src/test/java/com/tiny/abtest/ABTester.java
+++ core/src/test/java/com/tiny/abtest/ABTester.java
@@ -11,16 +11,18 @@ import java.util.Map;
  */
 public class ABTester {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         ABTestManager manager = new ABTestManager();
         ABTestTree model = manager.getABTestModel("xxx");
-        Map<ABTestTree.TreeNodeType, Object> map = new HashMap<>();
-        map.put(ABTestTree.TreeNodeType.CATEGORY,60);
-        map.put(ABTestTree.TreeNodeType.CITY,20);
-        map.put(ABTestTree.TreeNodeType.ID,10);
-        List<String> decisions = model.mkDecision(map);
-        for (String dec : decisions) {
-            System.out.println(dec);
+        if (null != model) {
+            Map<ABTestTree.TreeNodeType, Object> map = new HashMap<>();
+            map.put(ABTestTree.TreeNodeType.CATEGORY, 60);
+            map.put(ABTestTree.TreeNodeType.CITY, 20);
+            map.put(ABTestTree.TreeNodeType.ID, 10);
+            List<String> decisions = model.mkDecision(map);
+            for (String dec : decisions) {
+                System.out.println(dec);
+            }
         }
     }
 
